45e29db2e70c9c233154618f45b7e19b56d56d67
Svetoslav Nedelchev <snedelchev@isimarkets.com>
2025-07-05 20:25:20 +0300

    create initial components

---
+++ client/license-management-client/src/components/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+import SubscriptionPlanComponent from './SubscriptionPlanComponent';
+
+function Dashboard() {
+    return (
+        <div className='container sm:bg-white mx-auto'>
+            <SubscriptionPlanComponent />
+        </div>
+    );
+}
+
+export default Dashboard;
---
+++ client/license-management-client/src/components/SubscriptionPlanComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function SubscriptionPlanComponent() {
+    return (
+        <div>
+            <h1>Subscription Plan</h1>
+        </div>
+    );
+}
+
+export default SubscriptionPlanComponent;
