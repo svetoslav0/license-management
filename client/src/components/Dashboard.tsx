8738f9583319dce66bd4a8e298ae16858f31465d
Svetoslav Nedelchev <snedelchev@isimarkets.com>
2025-07-06 14:56:42 +0300

    moved api request creation in main component

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+import { PlanResponse } from '../api/ApiClientGenerated';
+import { apiClient } from '../api/apiClient';
 
 import SubscriptionPlanComponent from './SubscriptionPlanComponent';
 
 function Dashboard() {
+    const [currentPlan, setCurrentPlan] = useState<PlanResponse | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        apiClient.getCurrentPlan()
+            .then((plan: PlanResponse) => {
+                setCurrentPlan(plan);
+                setIsLoading(false);
+            })
+            .catch(error => {
+                console.log(error);
+                setIsLoading(false);
+            });
+    }, []);
+    
+    if (isLoading) return <>Loading...</>;
+
     return (
+        currentPlan &&
         <div className='container sm:bg-white mx-auto'>
-            <SubscriptionPlanComponent />
+            <SubscriptionPlanComponent currentPlan={currentPlan} />
         </div>
     );
 }
--- client/src/components/SubscriptionPlanComponent.tsx
+++ client/src/components/SubscriptionPlanComponent.tsx
@@ -1,25 +1,10 @@
-import React, {useEffect, useState} from 'react';
-
 import { PlanResponse } from '../api/ApiClientGenerated';
-import { apiClient } from '../api/apiClient';
 
-function SubscriptionPlanComponent() {
-    const [currentPlan, setCurrentPlan] = useState<PlanResponse | null>(null);
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        apiClient.getCurrentPlan()
-            .then((plan: PlanResponse) => {
-                setCurrentPlan(plan);
-                setIsLoading(false);
-            })
-            .catch(error => {
-                console.log(error);
-                setIsLoading(false);
-            });
-    }, []);
-
-    if (isLoading) return <div>Loading . . .</div>;
+function SubscriptionPlanComponent({
+        currentPlan
+    }: {
+        currentPlan: PlanResponse;
+    }) {
 
     return (
         currentPlan &&
